91b07553056f0b4969deecaac3b8d3894b4a27e3
talrez11 <tal@way2cu.com>
2015-01-19 18:17:15 +0200

    Added changes to main.js

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -17,6 +17,27 @@ function on_site_load() {
 				.setInterval(5000)
 				.setWrapAround(true);
 
+	//form configuration
+	var container=$('<div class="first">');
+	var container2=$('<div class="second">');
+
+	$('form label').eq(0).detach().appendTo(container);
+	$('form label').eq(1).detach().appendTo(container);
+	$('form label').eq(2).detach().appendTo(container);
+	$('form label').eq(3).detach().appendTo(container);
+	$('form label').eq(4).detach().appendTo(container);
+	$('form label').eq(5).detach().appendTo(container);
+
+
+	$('form label').eq(6).detach().appendTo(container2);
+	$('form label').eq(7).detach().appendTo(container2);
+	$('form label').eq(8).detach().appendTo(container2);
+	$('form label').eq(9).detach().appendTo(container2);
+
+	$(container).appendTo('form');
+	$(container2).appendTo('form');
+
+
 }
 
 $(on_site_load);
