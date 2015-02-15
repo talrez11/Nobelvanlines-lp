be3179cd23adcbbf75db04296ee95649faa5c142
talrez11 <tal@way2cu.com>
2015-02-15 12:08:24 +0200

    Added changes to main.js

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -14,6 +14,7 @@ function on_site_load() {
  	Caracal.slides = new PageControl('div#slides', 'div.slide');
  	$('div.form_container form:first()').css('display','block');
  	$('div.form_container form:last()').css('display','none');
+
 	// configure page control for sliders
 	Caracal.slides
 				.setInterval(5000)
@@ -25,6 +26,9 @@ function on_site_load() {
 		$('a.arrow.previous').css('backgroundColor','#94a4a5');
 		$('div.form_container form:first()').css('display','block');
 		$('div.form_container form:last()').css('display','none');
+		$('div#slides div.inner_wrap div.inner_form div.greeting').css('z-index','-1');
+		$('div#slides div.inner_wrap div.inner_form div.greeting').css('opacity','0');
+
 	});
 
 	$('a.arrow.previous').click(function(event) {
@@ -32,6 +36,8 @@ function on_site_load() {
 		$('a.arrow.next').css('backgroundColor','#94a4a5');
 		$('div.form_container form:first()').css('display','none');
 		$('div.form_container form:last()').css('display','block');
+		$('div#slides div.inner_wrap div.inner_form div.greeting').css('z-index','-1');
+		$('div#slides div.inner_wrap div.inner_form div.greeting').css('opacity','0');
 	});
 
 	function validateForm1() {
@@ -54,13 +60,11 @@ function on_site_load() {
 		}
 
 		if(date == null || date == "" ) {
-			$('fieldset input[name="date"]').attr('placeholder','Enter City Name');
 			$('fieldset input[name="date"]').css('border','1px solid red');
 			return false;
 		}
 
 		if(select == null || select == "" ) {
-			$('fieldset select[name="home_size"]').attr('placeholder','Enter City Name');
 			$('fieldset select[name="home_size"]').css('border','1px solid red');
 			return false;
 		}
@@ -89,13 +93,11 @@ function on_site_load() {
 		}
 
 		if(date == null || date == "" ) {
-			$('fieldset input[name="country_date"]').attr('placeholder','Enter City Name');
 			$('fieldset input[name="country_date"]').css('border','1px solid red');
 			return false;
 		}
 
 		if(select == null || select == "" ) {
-			$('fieldset select[name="country_home_size"]').attr('placeholder','Enter City Name');
 			$('fieldset select[name="country_home_size"]').css('border','1px solid red');
 			return false;
 		}
@@ -108,6 +110,7 @@ function on_site_load() {
 	$('form:first() fieldset.visible button').click(function() {
 		if(validateForm1()){
 	 	 $(this).css('display','none');
+
 		}
 	});
 
@@ -119,70 +122,26 @@ function on_site_load() {
 		}
 	});
 
+
+	// Form Close
+
+	$('form:first()').on('dialog-show', function() {
+			$('form').hide();
+			$('div.send').hide();
+			$('div.inner_form div.greeting').css('opacity','1');
+			$('div.inner_form div.greeting').css('z-index','2');
+			return false;
+		});
+	$('form:last()').on('dialog-show', function() {
+			$('form').hide();
+			$('div.send').hide();
+			$('div.inner_form div.greeting').css('opacity','1');
+			$('div.inner_form div.greeting').css('z-index','2');
+			return false;
+		});
+
 }
 
 $(on_site_load);
 
-// function Form(container) {
-// 	var self = this;
 
-// 	self.container = container;
-// 	self.fields = null;
-// 	self.first_section = $('<div class="first">');
-// 	self.second_section = $('<div class="second">');
-// 	self.goBtn = $('<input type="button" value="GO">');
-// 	self.backBtn = $('<input type="button" value="BACK">');
-
-// 	self._init = function() {
-// 		self.fields = self.container.find('label');
-// 		console.log(self.fields);
-
-// 		for(var i = 0, length = self.fields.length; i <= length; i++) {
-// 			var field = self.fields.eq(i);
-
-// 			field.detach();
-// 			if (i <= 5)
-// 				field.appendTo(self.first_section); else
-// 				field.appendTo(self.second_section);
-// 		}
-
-// 		self.goBtn.addClass('btn').appendTo(self.container.find('div.controls'));
-// 		self.goBtn.click(self._handle_next);
-// 		self.backBtn.addClass('backBtn').appendTo(self.container.find('div.controls'));
-// 		self.backBtn.click(self._handle_back);
-
-// 		self.container.prepend(self.second_section);
-// 		self.container.prepend(self.first_section);
-// 	};
-
-// 	/**
-// 	 * Handle clicking on next button.
-// 	 *
-// 	 * @param object event
-// 	 */
-// 	self._handle_next = function(event){
-// 		self.first_section.animate({left: '100%'}, 300);
-// 		self.second_section.animate({left: '0%'}, 300, function() {
-// 			self.backBtn.animate({opacity: '1'});
-// 			self.goBtn.animate({display: 'none',});
-// 			self.container.find('div.controls button').css('z-index','1');
-// 		});
-// 	};
-
-// 	/**
-// 	 * Handle clicking on previous button.
-// 	 *
-// 	 * @param object event
-// 	 */
-// 	self._handle_back = function(event) {
-// 		self.second_section.animate({left: '100%'}, 300);
-// 		self.first_section.animate({left: '0%'}, 300, function() {
-// 			self.backBtn.animate({opacity: '0'});
-// 			self.goBtn.animate({opacity: '1'});
-// 			self.container.find('div.controls button').css('z-index','0');
-// 		});
-// 	};
-
-// 	// finalize object
-// 	self._init();
-// }
